b7c3de0f8cec0130eec9d032faf01ea0e630b495
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-07 11:36:39 -0800

    generating the markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,33 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License 📛
+      Copyright © ${license}. All rights reserved. 
+      
+      Licensed under the ${license} license.`
+
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ![License Badge](https://img.shields.io/github/license/${data.github}/${data.repo})
+  ${renderLicenseBadge(data.license)}
+  ## Github URL
+  [${data.github}](https://github.com/${data.github}/)
   ## Description
   ${data.description}
   ## Table of Contents
   * [Installation](#installation)
   * [Usage](#usage)
-  * [Contribution](#contribution)
+  ${renderLicenseLink(data.license)}
+  * [Contribution](#credits)
   * [Tests](#tests)
   * [Questions](#questions)
   * [License](#license)
   ## Installation
   ${data.installation}
   ## Usage
-  ${data.usage}
+  ${renderLicenseSection(data.license)}
   ## Contribution
   ${data.contribution}
   ## Tests
